ef10df12c3714060189d3599c2607f452a23967b
leecurrent04 <leecurrent04@gmail.com>
2023-04-25 22:34:59 +0900

    230425

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # absent-checking-automation
-2022 Yeoncho Project
+
+![img_github.png](./resource/img/img_github.png)
+
+2022 Yeoncho Project <br>
+
+## 느낀점
+
+대량의 데이터를 정해진 규격으로 문서화하는 경우, Office 프로그램(MS Word, Hancom etc)을 이용해 html 형식의 양식을 만드는 것이 편리하다.
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import sys
 import os
 import os.path
 import shutil
-import platform
 
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
@@ -38,10 +37,10 @@ class WindowMainClass(QMainWindow, form_main):
 
         self.Button_file.setIcon(QIcon('resource/img/document-open-symbolic.svg'))
 
-        self.Button_file.clicked.connect(self.table_file_load)
-        self.Button_run.clicked.connect(self.run_making_pdf)
+        self.Button_file.clicked.connect(self.load_table_file)
+        self.Button_run.clicked.connect(self.make_html)
 
-    def table_file_load(self):
+    def load_table_file(self):
         # return data : ('[file path]','file type')
         ori_file_path, _ = QFileDialog.getOpenFileName(
             self,
@@ -51,6 +50,7 @@ class WindowMainClass(QMainWindow, form_main):
         )
         print(ori_file_path)
 
+        # make directory name
         if ori_file_path != "":
             global path_csv
             global path_directory
@@ -63,105 +63,135 @@ class WindowMainClass(QMainWindow, form_main):
                 ori_file_name.split('.')[0]
             )
 
+            # check if same directory is already existing
             if os.path.isdir(path_directory) == 1:
-                print("error: same directory")
-                self.statusBar.showMessage("There are already same folder! Try 1 minute later.")
-            else:
-                os.mkdir(path_directory)
+                i = 1
 
-                shutil.copy("./resource/form_style.css", "%s/form_style.css" % path_directory)
+                while os.path.isdir("%s(%d)" % (path_directory, i)) == 1:
+                    i += 1
 
-                # xlsx to csv
-                xlsx = pandas.read_excel(ori_file_path)
-                path_csv = "%s/%s.csv" % (path_directory, ori_file_name.split(".")[0])
-                xlsx.to_csv(path_csv)
+                path_directory = "%s(%d)" % (path_directory, i)
 
+            if (0 == make_result_directory(ori_file_path, ori_file_name)):
                 self.statusBar.showMessage("%s is ready." % ori_file_name)
 
-    def run_making_pdf(self):
+    def make_html(self):
         global path_csv
         global path_directory
-        file_data = ""
 
-        teacher = self.lineEdit.text()
-        tmp_grade = self.SBox_grade.value()
-        tmp_class = self.SBox_class.value()
+        # check if csv file is existing
+        if path_csv == "":
+            self.statusBar.showMessage("Load file first")
 
-        self.statusBar.showMessage("start")
+        else:
+            file_data = ""
 
-        # 학생 출결 정보 파일을 불러옴
-        with open(path_csv, 'r', encoding='UTF-8') as table:
+            teacher = self.lineEdit.text()
+            num_grade = self.SBox_grade.value()
+            num_class = self.SBox_class.value()
 
-            # 한 줄씩 처리
-            for n in table.readlines()[1:]:
-                # variable
-                tmp_type = n[:-1].split(",")[4]
+            self.statusBar.showMessage("start")
 
-                if tmp_type[-2:] == "결석" or tmp_type == "출석인정조퇴":
-                    if tmp_type != "미인정결석":
+            with open(path_csv, 'r', encoding='UTF-8') as table:  # 학생 출결 정보 파일을 불러옴
 
-                        # n[:-1] : delete \n
-                        tmp_split_data = n[:-1].split(",")
+                for line in table.readlines()[1:]:  # Process one line by one line
 
-                        # if reason include ","
-                        if len(tmp_split_data) > 6:
-                            _, date, number, name = tmp_split_data[:4]
-                            reason = ', '.join(str(s) for s in tmp_split_data[5:])
-                            reason = reason.replace('"', "")
-                        else:
-                            _, date, number, name, _, reason = tmp_split_data
+                    date, number, name, type, reason = split_line_data(line)
 
-                        year, month, day = date.split(".")
+                    if (1 == check_absent_type(type, reason)):
+                        print(line, end="")
 
-                        if reason != "가정학습" and reason != "가정 학습":
+                        year, month, day = date.split(".")  # split date
 
-                            print(n, end="")
+                        absent_marking = ["　", "　", "　"]  # check absent type
+                        index_of_absent = ["출석인정결석", "질병결석", "출석인정조퇴"].index(type)
+                        absent_marking[index_of_absent] = "Ｏ"
 
-                            # load resource file
-                            with open("./resource/form_mid.html", 'r', encoding='UTF-8') as form:
-                                tmp_data = form.read()
+                        name = (len(name) < 6) if ("　" * (5 - len(name)) + name) else name
 
-                            # check absent type
-                            if tmp_type == "출석인정결석":
-                                codeA = "Ｏ"; codeB = "　"; codeC = "　"
-                            elif tmp_type == "질병결석":
-                                codeA = "　"; codeB = "Ｏ"; codeC = "　"
-                            elif tmp_type == "출석인정조퇴":
-                                codeA = "　"; codeB = "　"; codeC = "Ｏ"
+                        if len(teacher) < 6:
+                            teacher = "　" * (5 - len(teacher)) + teacher
 
-                            if len(name) < 6 :
-                                tmp_name = "　"*(5-len(name)) + name
-                            else:
-                                tmp_name = name
+                        # input data
+                        old_data = ["$yr", "$mo", "$dy",
+                                    "$grade", "$class", "$number", "$name",
+                                    "$reason", "$state", "$teacher",
+                                    "$a", "$b", "$c"
+                                    ]
 
-                            if len(teacher) < 6 :
-                                teacher = "　"*(5-len(teacher)) + teacher
+                        new_data = [year, month, day,
+                                    num_grade, num_class, number, name,
+                                    reason, type[-2:], teacher,
+                                    absent_marking[0], absent_marking[1], absent_marking[2]
+                                    ]
 
-                            # input data
-                            old_data = ["$yr", "$mo", "$dy",
-                                        "$grade", "$class", "$number", "$name",
-                                        "$reason", "$state", "$teacher",
-                                        "$a", "$b", "$c"
-                                        ]
+                        # load resource file
+                        with open("./resource/form_mid.html", 'r', encoding='UTF-8') as form:
+                            tmp_data = form.read()
 
-                            new_data = [year, month, day,
-                                        tmp_grade, tmp_class, number, tmp_name,
-                                        reason, tmp_type[-2:], teacher,
-                                        codeA, codeB, codeC
-                                        ]
-
-                            for i in range(0,len(old_data)):
+                            for i in range(0, len(old_data)):
                                 tmp_data = tmp_data.replace(str(old_data[i]), str(new_data[i]))
 
                             file_data += tmp_data
                             file_data += "<div style='page-break-before:always'></div>\n"
 
-        self.statusBar.showMessage("done")
+            # last page fix
+            file_data = file_data[:-len("<div style='page-break-before:always'></div>\n")]
 
-        with open("%s/all.html" % path_directory, 'w', encoding='UTF-8') as file:
-            with open("./resource/form.html", 'r', encoding='UTF-8') as html:
-                tmp_file_data = html.read() % file_data
-                file.write(tmp_file_data)
+            # copy html <head> data to all.html from resource
+            with open("%s/all.html" % path_directory, 'w', encoding='UTF-8') as file:
+                with open("./resource/form.html", 'r', encoding='UTF-8') as html:
+                    tmp_file_data = html.read() % file_data
+                    file.write(tmp_file_data)
+
+            self.statusBar.showMessage("done")
+
+
+def make_result_directory(file_path, file_name):
+    global path_csv
+    global path_directory
+
+    os.mkdir(path_directory)
+
+    shutil.copy("./resource/form_style.css", "%s/form_style.css" % path_directory)
+
+    # xlsx to csv
+    xlsx = pandas.read_excel(file_path)
+    path_csv = "%s/%s.csv" % (path_directory, file_name.split(".")[0])
+    xlsx.to_csv(path_csv)
+
+    return 0
+
+
+def check_absent_type(type, reason):
+    if type[-2:] == "결석" or type == "출석인정조퇴":
+        if type != "미인정결석":
+            if reason != "가정학습" and reason != "가정 학습":
+                return 1
+
+    return 0
+
+
+def split_line_data(line):
+    split_data = line[:-1].split(",")           # line[:-1] : delete "\n"
+
+    if len(split_data) > 6:                     # check if reason included ","
+        _, date, number, name, type = split_data[:5]
+        reason = ', '.join(str(s) for s in split_data[5:])
+
+        if reason[-1:] == '"':
+            reason = reason[1:-1]               # delete "" that made when xlsx to csv
+        else:                                   # when reason was had "\n" before convert csv
+            reason = reason[1:]
+
+        return date, number, name, type, reason
+
+    elif len(split_data) == 6:
+        _, date, number, name, type, reason = split_data
+        return date, number, name, type, reason
+
+    else:                                       # when previous reason had \n
+        return 0, 0, 0, "0", "0"
 
 
 if __name__ == "__main__":
